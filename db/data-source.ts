cc13d057d05dff455de5f6572cf1741c030052e3
Epan Lauw <epanlauw6@gmail.com>
2025-03-06 18:44:06 +0700

    modify to env file

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,11 +1,14 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { config } from 'dotenv';
+
+config();
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'admin',
-  database: 'ecommerce-nest',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [],
   migrations: [],
   logging: false,
