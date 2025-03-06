397245279bc1fcf09b23b7a2ee978b6f5978733c
Epan Lauw <epanlauw6@gmail.com>
2025-03-06 19:28:15 +0700

    add migraton schema to create table

--- db/data-source.ts
+++ db/data-source.ts
@@ -10,9 +10,9 @@ export const dataSourceOptions: DataSourceOptions = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations: [],
+  migrations: ['dist/db/migrations/*{.ts,.js}'],
   logging: true,
-  synchronize: true,
+  synchronize: false,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
