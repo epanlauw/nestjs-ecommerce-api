294ad9448799501a6a165e533be2d56db9310180
Epan Lauw <epanlauw6@gmail.com>
2025-04-02 10:29:45 +0700

    Improve authorization guard with mixin

--- src/utility/guards/authorization.guard.ts
+++ src/utility/guards/authorization.guard.ts
@@ -1,28 +1,43 @@
 import {
   CanActivate,
   ExecutionContext,
-  Injectable,
+  mixin,
   UnauthorizedException,
 } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
 
-@Injectable()
-export class AuthorizeGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+// @Injectable()
+// export class AuthorizeGuard implements CanActivate {
+//   constructor(private readonly reflector: Reflector) {}
 
-  canActivate(context: ExecutionContext): boolean {
-    const allowedRoles = this.reflector.get<string[]>(
-      'allowedRoles',
-      context.getHandler(),
-    );
-    const request = context.switchToHttp().getRequest<Request>();
-    const result = request?.currentUser?.roles
-      .map((role: string) => allowedRoles.includes(role))
-      .find((val: boolean) => val === true);
-    if (result) return true;
-    throw new UnauthorizedException(
-      'You are not authorized to access this resource',
-    );
+//   canActivate(context: ExecutionContext): boolean {
+//     const allowedRoles = this.reflector.get<string[]>(
+//       'allowedRoles',
+//       context.getHandler(),
+//     );
+//     const request = context.switchToHttp().getRequest<Request>();
+//     const result = request?.currentUser?.roles
+//       .map((role: string) => allowedRoles.includes(role))
+//       .find((val: boolean) => val === true);
+//     if (result) return true;
+//     throw new UnauthorizedException(
+//       'You are not authorized to access this resource',
+//     );
+//   }
+// }
+export const AuthorizeGuard = (allowedRoles: string[]) => {
+  class RolesGuardMixin implements CanActivate {
+    canActivate(context: ExecutionContext): boolean {
+      const request = context.switchToHttp().getRequest<Request>();
+      const result = request?.currentUser?.roles
+        .map((role: string) => allowedRoles.includes(role))
+        .find((val: boolean) => val === true);
+      if (result) return true;
+      throw new UnauthorizedException(
+        'You are not authorized to access this resource',
+      );
+    }
   }
-}
+  const guard = mixin(RolesGuardMixin);
+  return guard;
+};
