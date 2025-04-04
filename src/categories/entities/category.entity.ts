4a7b4e16fec0e4a857d0e8ab961db1412c6ec40d
Epan Lauw <epanlauw6@gmail.com>
2025-04-04 19:41:20 +0700

    category resource functionality

---
+++ src/categories/entities/category.entity.ts
@@ -0,0 +1,30 @@
+import { UserEntity } from 'src/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'categories' })
+export class CategoryEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(() => UserEntity, (user) => user.categories)
+  addedBy: UserEntity;
+}
